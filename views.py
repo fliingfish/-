8c512c2c8cc1ea55263047020631df4bd8228fda
1994328785@qq.com <201819154321a>
2019-10-30 23:20:13 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,6 +1,10 @@
-from djsngo.http import HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
 
 def index(request):
     return HttpResponse('index')
+
+def longin(request):
+    return redirect('/index')
